e025821ace6f9470279b04bd946148431daf73c8
Hiroyuki Wada <wadahiro@gmail.com>
2019-10-31 00:56:34 +0900

    My custom keymap

--- keyboards/choco60/keymaps/default/keymap.c
+++ keyboards/choco60/keymaps/default/keymap.c
@@ -23,13 +23,28 @@ enum layer_names {
 
 #define KC_FN MO(_FN)
 
+enum custom_keycodes {
+    M_HNZN_TGL = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch(keycode) {
+            case M_HNZN_TGL:
+                SEND_STRING(SS_LALT("`"));
+                return false;
+        }
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH, KC_GRAVE,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSPACE,
     KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
     KC_LSHIFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_FN,
-    KC_LALT,  KC_LGUI, KC_SPACE, KC_SPACE, KC_SPACE, KC_RGUI, KC_RALT
+    KC_LGUI, KC_LALT, KC_SPACE, KC_SPACE, KC_SPACE, M_HNZN_TGL, KC_RGUI
   ),
   [_FN] = LAYOUT(
     _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INSERT, KC_DELETE,
