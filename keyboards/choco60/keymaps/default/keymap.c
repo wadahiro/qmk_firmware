84ce4b421c4ec5cd3c32b2bcd190059825a8e721
Hiroyuki Wada <wadahiro@gmail.com>
2021-08-03 11:44:46 +0900

    Use standard layout because using karabiner and add KC_LCTRL

--- keyboards/choco60/keymaps/default/keymap.c
+++ keyboards/choco60/keymaps/default/keymap.c
@@ -23,33 +23,73 @@ enum layer_names {
 
 #define KC_FN MO(_FN)
 
-enum custom_keycodes {
-    M_HNZN_TGL = SAFE_RANGE,
-};
+#define MT_LGUI MT(MOD_LGUI, KC_LANG2)
+#define MT_RGUI MT(MOD_RGUI, KC_LANG1)
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch(keycode) {
-            case M_HNZN_TGL:
-                SEND_STRING(SS_LALT("`"));
-                return false;
-        }
-    }
-    return true;
-};
+// enum custom_keycodes {
+//     M_LGUI = SAFE_RANGE,
+//     M_RGUI
+// };
+
+// static bool lgui_pressed = false;
+// static bool rgui_pressed = false;
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//     switch(keycode) {
+//       case M_LGUI:
+//         if (record->event.pressed) {
+//           lgui_pressed = true;
+//           register_code(KC_LGUI);
+
+//         } else {
+//           if (lgui_pressed) {
+//             register_code(KC_LANG2);
+//             unregister_code(KC_LANG2);
+//           }
+//           lgui_pressed = false;
+//           unregister_code(KC_LGUI);
+//         }
+//         return false;
+//         break;
+//       case M_RGUI:
+//         if (record->event.pressed) {
+//           rgui_pressed = true;
+//           register_code(KC_RGUI);
+
+//         } else {
+//           if (rgui_pressed) {
+//             register_code(KC_LANG1);
+//             unregister_code(KC_LANG1);
+//           }
+//           rgui_pressed = false;
+//           unregister_code(KC_RGUI);
+//         }
+//         return false;
+//         break;
+//       default:
+//         if (record->event.pressed) {
+//           // reset the flag
+//           lgui_pressed = false;
+//           rgui_pressed = false;
+//         }
+//         break;
+//     }
+        
+//     return true;
+// };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH, KC_GRAVE,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSPACE,
-    KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
+    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
     KC_LSHIFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_FN,
-    KC_LGUI, KC_LALT, KC_SPACE, KC_SPACE, KC_SPACE, M_HNZN_TGL, KC_RGUI
+    KC_LALT, MT_LGUI, KC_SPACE, KC_SPACE, KC_SPACE, MT_RGUI, KC_RALT
   ),
   [_FN] = LAYOUT(
     _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INSERT, KC_DELETE,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_UP, KC_RBRACKET, KC_BSPACE,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENTER,
+    KC_RCTRL, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENTER,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_PGDOWN, KC_DOWN, KC_RSHIFT, KC_FN,
     _______, _______, _______, _______, _______, KC_STOP, _______
   )
